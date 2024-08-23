fc4f12ca61145e3008ba4786c11acee774713f65
Luis Pedrinho <lpedrinho@suthub.com>
2024-08-23 15:36:07 -0300

    Divisao dos parametros de entrada em tabelas separadas.

--- gestao_ativos/models.py
+++ gestao_ativos/models.py
@@ -1,20 +1,48 @@
 from django.db import models
 from users.models import User
 
-class Ativos(models.Model):
-    id_ativo = models.IntegerField(primary_key=True)
+class Ativo(models.Model):
+    id_ativo = models.AutoField(primary_key=True)
     nome_ativo = models.CharField(max_length=20, unique=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_modificacao = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.nome_ativo
 
 
-class AtivosMonitorados(models.Model):
-    id_ativo_monitorado = models.IntegerField(primary_key=True)
-    id_ativos = models.ManyToManyField(Ativos)
-    id_usuario = models.ManyToManyField(User)
-    class_tunel = models.CharField(max_length=10)
+class ClassificacaoTunel(models.Model):
+    id_class_tunel = models.AutoField(primary_key=True)
+    classificacao = models.CharField(unique=True, max_length=50)
+    
+    
+    def __str__(self):
+        return self.classificacao
+
+class TipoTunel(models.Model):
+    id_tipo_tunel = models.AutoField(primary_key=True)
     tipo_tunel = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.tipo_tunel
+
+class TipoProtecao(models.Model):
+    id_tipo_protecao = models.AutoField(primary_key=True)
     tipo_protecao = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.tipo_protecao
+
+class AtivosMonitorado(models.Model):
+    id_ativo_monitorado = models.AutoField(primary_key=True)
+    ativo = models.ManyToManyField(Ativo)
+    usuario = models.ManyToManyField(User)
+    class_tunel = models.ManyToManyField(ClassificacaoTunel)
+    tipo_tunel = models.ManyToManyField(TipoTunel)
+    tipo_protecao = models.ManyToManyField(TipoProtecao)
     preco_base = models.CharField(max_length=20)
     peridiocidade = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_modificacao = models.DateTimeField(auto_now=True)
